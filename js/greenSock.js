6bf5b6355b186f9ed55c4e256ff4ead5386f2cd2
olivier villevieille <oliviervillevieille@hotmail.fr>
2021-09-21 15:27:26 +0200

    modifs animations accueil

--- js/greenSock.js
+++ js/greenSock.js
@@ -5,27 +5,15 @@ const titrePrincipal = document.querySelector("h1");
 const titreSecondaire = document.querySelector("header h2");
 
 window.addEventListener("load", () => {
-  const TL = gsap.timeline({ delay: 0.3, paused: true });
+  const TL = gsap.timeline();
 
-  TL.to(navigation, { autoAlpha: 1, y: 0, ease: "power2.in" })
-    .staggerFrom(lienNav, 1, { opacity: 0, ease: "power2.out" }, 0.5)
+  TL.to(navigation, { autoAlpha: 1, y: 0, ease: "power3.in" })
+    .staggerTo(lienNav, 2, { autoAlpha: 1, y: 0, ease: "power2.out" }, 0.5)
 
-    .staggerFrom(
-      titrePrincipal,
-      2,
-      { opacity: 0, ease: "power2.out" },
-      0.8,
-      "-=1"
-    )
+    .to(titrePrincipal, 1, { autoAlpha: 1, y: 0, ease: "power2.out" })
 
-    .staggerFrom(
-      titreSecondaire,
-      2,
-      { opacity: 0, ease: "power2.ease" },
-      1.4,
-      "-=1"
-    )
-    .staggerFrom(arrow, 3, { opacity: 0, ease: "power2.ease" }, 1.8, "-=1");
+    .to(titreSecondaire, 1, { autoAlpha: 1, y: 0, ease: "power2.out" })
+    .to(arrow, 2, { autoAlpha: 1, ease: "power3.out" });
 
   TL.play();
 });
